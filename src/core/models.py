5b24c47e9169a8364f31f5320c199728cd6697f8
EldarKhayarov <hayaroff.eldar@yandex.ru>
2019-10-01 20:46:19 +0400

    Renamed fields

--- src/core/models.py
+++ src/core/models.py
@@ -3,8 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class TimestampedModel(models.Model):
-    created_ad = models.DateTimeField(verbose_name=_('Date and time of creating'), auto_now_add=True)
-    updated_ad = models.DateTimeField(verbose_name=_('Date and time of updating'), auto_now=True)
+    created_at = models.DateTimeField(verbose_name=_('Date and time of creating'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_('Date and time of updating'), auto_now=True)
 
     class Meta:
         abstract = True
